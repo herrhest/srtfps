54e4fc8d74eff3d9a9d6095c21e73920860d0381
ericthewolf <34064180+herrhest@users.noreply.github.com>
2020-06-13 15:50:58 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# srtfps
+# srtfps.js
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Contributing](#contributing)
+
+## About <a name = "about"></a>
+
 Node JS CLI program to resync srt frames per seconds. Uses "subtitle" node js.
+
+## Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine.
+
+### Prerequisites
+
+What things you need to install the software and how to install them:
+You only need Node jS and some subtitle files in `.srt` format you need to convert the fps of.
+
+### Installing
+
+1. Clone the repository.
+2. Open your terminal of choice and run `npm install`. This will install the project's dependencies.
+
+
+## Usage <a name = "usage"></a>
+
+1. in the project directory, run
+
+```console
+$ node srtfps.js <filename> <current fps> <desired fps>
+```
+
+## Contributing <a name = "contributing"></a>
+
+This is a really basic poc of srt fps conversion. Feel free to make it better or use the "alghoritm" in other projects.
---
+++ srtfps.js
@@ -0,0 +1,60 @@
+const Subtitle = require('subtitle')
+const fs = require('fs')
+var filenameInput = process.argv[2];
+var fpsFromInput = process.argv[3];
+var fpsToInput = process.argv[4];
+
+if (filenameInput == null) {
+    console.log("Enter a valid subtitle to convert.\nExample: $ node index.js <filename> <from fps integer> <to fps integer>")
+    return process.exit(22);
+}
+
+
+if (fpsFromInput == '23' && fpsToInput == '25') {
+    fpsFromInput = fpsFromInput.toString()
+    fpsToInput = fpsToInput.toString()
+
+
+    fs.readFile(filenameInput, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err + "\nEnter a valid subtitle file.");
+        }
+
+        //parse the srt to ms
+        var subarray = Subtitle.parse(data)
+
+        //speed the shit up  
+        subarray.forEach(fpsShift23to25fps)
+
+        //remake the ms to string
+        var substring = Subtitle.stringify(subarray)
+
+        //print the string to new file
+        fs.writeFile(appendToFilename(filenameInput, "25"), substring, function (err) {
+            if (err) return console.log(err);
+            console.log("Resynced the file " + filenameInput + " => " + appendToFilename(filenameInput, "25"))
+
+        });
+
+    });
+
+
+} else if (fpsFromInput == '25' && fpsToInput == '23') {
+    console.log("Coming soon...")
+} else {
+    console.log("Enter the fps you want to convert, ex 23, 25.\nExample: $ node index.js <filename> <from fps integer> <to fps integer>")
+}
+
+
+
+function fpsShift23to25fps(item, fps) {
+    const from23to25fps = 0.959040959040959;
+    item.start = (item.start * from23to25fps).toFixed(0)
+    item.end = (item.end * from23to25fps).toFixed(0)
+}
+
+function appendToFilename(filenameInput, string) {
+    var dotIndex = filenameInput.lastIndexOf(".");
+    if (dotIndex == -1) return filenameInput + string;
+    else return filenameInput.substring(0, dotIndex) + string + filenameInput.substring(dotIndex);
+}
